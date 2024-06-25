31def224f2a1ccde75fe4e432c9685eb99de99e5
mvetrici <hello0123495678@gmail.com>
2024-06-25 17:13:53 -0400

    update (unfinished)

--- makeModel.py
+++ makeModel.py
@@ -1,17 +1,99 @@
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import pandas as pd
-from dfObj import find_groupby
 import matplotlib.pyplot as plt
+from pd_helpers import get_label, add_col, get_col_count, get_label_list
+from folderProcessor import folderProcessor
+from dfObj import dfObj
+
+TRIPS = "bikeshare-ridership-2023"
+JANTRIPS = r"Bike share ridership 2023-01.csv"
+DATA = 'other-datasets-2023'
+
+def visualize_cost():
+    trips_folder = folderProcessor(TRIPS, 'test')
+    other_folder = folderProcessor(DATA)
+    df = trips_folder.combine_merge(other_folder).get_df()
+    df = add_col(df, ['weather', 'cost'])
+
+    df1 = df.groupby(['Start_Station_Id', 'End_Station_Id'])['cost'].mean().sort_values(ascending=False)
+    df2 = df.groupby(['Start_Station_Id', 'End_Station_Id'])['cost'].count().sort_values(ascending=False)
+    df = pd.merge(df1, df2, left_index=True, right_index=True)
+    df['utility'] = df['cost_x'] * df['cost_y']
+    # print(df)
+    # df = df.loc[lambda df: (df['Start_Station_Id'] == 7059) & (df['End_Station_Id'] == 7033) & (df['User_Type'] == 'Casual Member')]
+    #print(df)
+    df['utility'].plot(kind = 'hist', bins=20)
+    plt.show()
+    #print('done')
+    # pd.plot()
+
+def dura_dist(df: pd.DataFrame, groupby: str = None):
+    """Plots distribution of trip durations between 4 most common station pairs.
+    Does not modify dataframes."""    
+    if groupby and groupby not in df.columns:
+        df = add_col(df, [groupby]) # doesn't modify df
+    durations = df.filter(get_label_list(df, ['start station id', 'end station id', 'trip duration min', 'timeperiod']))
+    
+    # Find 4 most common station pairs
+    duration_counts = get_col_count(durations, new_col_name='count', bycol=['start station id', 'end station id'], new=True) #, keep=['trip duration min'])
+    check = duration_counts.sort_values('count', ascending=False)
+    o = list(check['Start_Station_Id'].iloc[0:4])
+    d = list(check['End_Station_Id'].iloc[0:4])
+
+    # Make historgram
+    fig, axs = plt.subplots(len(o)//2, 2, figsize=(6, 10)) 
+    # OPTION 2 (ONE plot) # plt.figure(figsize=(10, 6))
+    for i in range(len(o)):
+        start, end = o[i], d[i]
+        hist_i = durations.loc[lambda durations: (durations['Start_Station_Id'] == start) & (durations['End_Station_Id'] == end)]
+        # OPTION 1
+        # hist_i['Trip_Duration_min'].plot(kind = 'hist', bins=20, xlabel='Duration (min)')
+        # OPTION 2 (for ONE layered plot)
+        # plt.hist(hist_i['Trip_Duration_min'], bins=30, alpha=0.5, label=f"{start}-{end}")
+        # OPTION 3 (for multiple plots)
+        if groupby:
+            data = hist_i[['Trip_Duration_min', groupby]]
+            data.to_csv('test_w_gpt.csv', index=False)
+        else:
+            data = hist_i['Trip_Duration_min']
+        
+        axs[i%2][int(i in [0, 1])].hist(data, bins=30, alpha=0.5, color='b', label=f"{start}-{end}")
+        axs[i%2][int(i in [0, 1])].set_title(f'{start} and {end}')
+        # axs[i%2][int(i in [0, 1])].set_xlabel('Duration (min)')
+        axs[i%2][int(i in [0, 1])].set_ylabel('Frequency')
+        # axs[i].legend()
+    
+    # CSV
+    # data.to_csv('test_w_gpt.csv', index=False)
+    
+    # OPTION 2
+    # plt.xlabel('Duration (min)')
+    # plt.ylabel('Frequency')
+    # plt.title('Durations')
+    plt.legend()
+    plt.suptitle("Durations (mins)") # plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    trips_folder = folderProcessor(TRIPS, '09')
+    df = trips_folder.get_obj()
+    # dfs = trips_folder.get_dfs()
+    dura_dist(df.get_df(), 'timeperiod')
+
+    # for df in dfs:
+    #     dura_dist(df.get_df())
 
 
+# ///* Start class definition
+#  
 # Time periods: "Overnight", "AM", "Mid-day", "PM", "Evening"
 # ["Freezing", "Cold", "Cool", "Warm"]
 # "Winter", "Spring", "Summer", "Fall"
 class makeModel():
     """Make a model from a dataframe"""
     
-    def __init__(self, df: pd.DataFrame, predict: str, vars: list[str]):
+    def __init__(self, df: pd.DataFrame, predict: str, variables: list[str]):
         # print("Getting dummies...")
         # df = pd.get_dummies(df, columns=['User Type', 'Precip', 'Month_x', 'Time Period_start', "Time Period_end", "Temperature Range"], drop_first=True)
         # rename = {}
@@ -24,22 +106,26 @@ class makeModel():
         # df.reset_index(inplace=True)
         # # FIX ERROR!!!
         self.df = df.copy()
-        self.predict = find_groupby(df, predict)
-        self.new_vars = []
-        for var in vars:
-            var = find_groupby(df, var)
-            self.new_vars.append(var)
+        self.predict = get_label(df, predict)
+        self.vars = []
+        for var in variables:
+            var = get_label(df, var)
+            self.vars.append(var)
+        self.df.dropna(subset=self.vars + [self.predict])
+        self.df.reset_index(inplace=True)
         return
 
     def make_dot(self, x: str, y: str): 
-        x = find_groupby(self.df, x)
-        y = find_groupby(self.df, y)
+        x = get_label(self.df, x)
+        y = get_label(self.df, y)
         self.df.plot(x = x, y = y, kind = 'scatter')
         plt.show()
     
-    def make_lm(self):
-        formula = make_formula(self.predict, self.new_vars)
-        model = smf.mixedlm(self, data=self.df, groups=self.df.columns[0])
+    def make_lm(self, group: str):
+        grouping = get_label(self.df, group)
+        formula = make_formula(self.predict, self.vars)
+        vc_formula = {}
+        model = smf.mixedlm(formula, data=self.df, re_formula='1', groups=grouping)
         result = model.fit()
         print(result.summary())
         return
@@ -110,3 +196,11 @@ def make_formula(predict: str, variables: list[str]) -> str:
             ext = ''
         formula += variables[i] + ext
     return formula
+
+
+# more dump
+   #st7059_7033 = durations.loc[lambda df: (df['Start_Station_Id'] == 7059) & (df['End_Station_Id'] == 7033)]
+    #print(st7059_7033)
+    # print(durations.groupby(['Start_Station_Id', 'End_Station_Id'], dropna=False).nunique())
+    #st7059_7033['Trip_Duration_min'].plot(kind = 'hist', bins=20, xlabel='Duration (min)')
+    #plt.show()
